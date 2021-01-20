374aa5ebdd92090b3aef7dc95b7a5a6e9a99475f
SnO₂WMaN <sno2wman.official@gmail.com>
2021-01-20 23:52:17 +0900

    feat: UsersAPIへの接続を追加 (#25)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,44 @@
+import {RemoteGraphQLDataSource} from '@apollo/gateway';
 import {Module} from '@nestjs/common';
 import {ConfigModule, ConfigService} from '@nestjs/config';
-import {GraphQLGatewayModule} from '@nestjs/graphql';
+import {GATEWAY_BUILD_SERVICE, GraphQLGatewayModule} from '@nestjs/graphql';
 import graphqlConfig from './graphql/graphql.config';
+
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  async willSendRequest({request, context}: any) {
+    const authorization = context?.req?.headers?.authorization;
+    if (authorization) request.http.headers.set('authorization', authorization);
+  }
+}
+
+@Module({
+  providers: [
+    {
+      provide: AuthenticatedDataSource,
+      useValue: AuthenticatedDataSource,
+    },
+    {
+      provide: GATEWAY_BUILD_SERVICE,
+      inject: [AuthenticatedDataSource],
+      useFactory: (AuthenticatedDataSource) => {
+        return ({url}: {name: unknown; url: unknown}) =>
+          new AuthenticatedDataSource({url});
+      },
+    },
+  ],
+  exports: [GATEWAY_BUILD_SERVICE],
+})
+class BuildServiceModule {}
 
 @Module({
   imports: [
     GraphQLGatewayModule.forRootAsync({
-      imports: [ConfigModule.forFeature(graphqlConfig)],
-      inject: [ConfigService],
+      imports: [ConfigModule.forFeature(graphqlConfig), BuildServiceModule],
+      inject: [ConfigService, GATEWAY_BUILD_SERVICE],
       useFactory: async (configs: ConfigService) => ({
         server: {
           cors: true,
+          context: ({req}) => ({req}),
         },
         gateway: {
           serviceList: [
@@ -20,6 +48,7 @@ import graphqlConfig from './graphql/graphql.config';
               url: configs.get<string>('graphql.bookcoverUrl'),
             },
             {name: 'search', url: configs.get<string>('graphql.searchUrl')},
+            {name: 'users', url: configs.get<string>('graphql.usersUrl')},
           ],
         },
       }),
--- src/graphql/graphql.config.ts
+++ src/graphql/graphql.config.ts
@@ -4,4 +4,5 @@ export default registerAs('graphql', () => ({
   booksUrl: process.env.BOOKS_API_URL!,
   bookcoverUrl: process.env.BOOKCOVER_API_URL!,
   searchUrl: process.env.SEARCH_API_URL!,
+  usersUrl: process.env.USERS_API_URL!,
 }));
